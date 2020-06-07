06a38c5b34cc2be63f0e40e4811fb0a2e526d471
anjali <asc656@nyu.edu>
2020-06-07 18:46:21 -0400

    Beech Game Unity

---
+++ Beech/Assets/Scripts/buttonManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class buttonManager : MonoBehaviour
+{
+    public void Load(string scene)
+    {
+        Debug.Log("loaded: " + scene);
+        SceneManager.LoadScene(scene);
+    }
+}
---
+++ Beech/Assets/Scripts/cameraScroll.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraScroll : MonoBehaviour
+{
+    public float speed = 0.1F;
+    void Update()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+            transform.Translate(-touchDeltaPosition.x * speed, 0, 0);
+        }
+    }
+}
